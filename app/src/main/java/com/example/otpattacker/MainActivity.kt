765c1d6cd47f2fab82730485544a81b563a492e7
rexstudiooriginal <rexstudioorg@gmail.com>
2026-08-28 22:53:07 +0700

    Create java/com/example/otpattacker/MainActivity.kt

---
+++ app/src/main/java/com/example/otpattacker/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.otpattacker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.otpattacker.ui.MainScreen
+import com.example.otpattacker.ui.theme.OTPAttackerTheme
+import com.example.otpattacker.viewmodel.OtpViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OTPAttackerTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    val viewModel: OtpViewModel = viewModel()
+                    MainScreen(viewModel = viewModel)
+                }
+            }
+        }
+    }
+}
