b0ff01b0b231f3126971e03d8c6dbed9ec129567
rexstudiooriginal <rexstudioorg@gmail.com>
2026-08-30 13:07:11 +0700

    Update pada 30-08-2026 13:07:11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.android.application") version "8.3.0" apply false
+    id("com.android.library") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false // <- PENTING: GANTI JADI 1.9.22
     id("com.google.dagger.hilt.android") version "2.48" apply false
 }
